82806a4978e56c393cf2005adaf3710e6c48d035
unknown <josefolofus@gmial.com>
2023-06-23 10:29:08 -0400

    Terminal

---
+++ TERMINAL/Git_Hub/examen.js
@@ -0,0 +1,5 @@
+function completenames(name,lastname,nickname){
+   completename= [name+lastname+nickname]; 
+   console.log('Mi nombre es +${completename[0]}+ y mi apellido es +${completename[1]}+pero me dicen +${completename[2]}')
+}
+completenames('jose','pabon','jossito');
