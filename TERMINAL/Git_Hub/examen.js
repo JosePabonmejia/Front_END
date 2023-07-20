a973fd047fedcfe96bee5f9cf5543cc8ff9bddb5
unknown <josefolofus@gmial.com>
2023-07-20 19:52:31 -0400

    cambios git

--- TERMINAL/Git_Hub/examen.js
+++ TERMINAL/Git_Hub/examen.js
@@ -3,3 +3,4 @@ function completenames(name,lastname,nickname){
    console.log('Mi nombre es +${completename[0]}+ y mi apellido es +${completename[1]}+pero me dicen +${completename[2]}')
 }
 completenames('jose','pabon','jossito');
+completenames('jose','pabon','jossito');
